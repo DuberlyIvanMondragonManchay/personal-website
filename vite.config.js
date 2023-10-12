19d45733a13e344f5a39e7143f44d31c54f29c38
DuberlyIvanMondragonManchay <duberly.mondragon@tecsup.edu.pe>
2023-10-12 16:53:34 -0500

    Add react router dom

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'http://duberlymondragon.com/'
+  base: 'https://duberlymondragon.com/'
 })
